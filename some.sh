d82ad3a30f25d5ac18f3b31470908de4752208b2
zrobysam <oleh.n@i.ua>
2020-05-09 19:39:46 +0300

    A new commit

---
+++ some.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+# some shit is here
+echo "Hi there, asshole!"      # and more shit is here
+# read -p "Type any shit: " new
+# read -sp "Type your fuckung password: " pass
+# read 
+# echo $REPLY
+
+# read this that
+# echo "And one shit more plz"
+# read that
+# clear
+# echo So your shit is $new and you password is $pass 
+
+# echo $1 $2 $3
+
+# args=("$@")
+
+# echo ${args[0]} ${args[1]} ${args[2]}
+
+# echo $@
+
+# echo $#
+
+tempp=some
+echo $tempp
+
+if [[ $tempp < som ]]
+then 
+    echo "yep"
+else
+    echo "nope"
+fi
